996b9996ea321b4fb2c7a6dc3191d5d2d50cf948
Tuhin Pal <tuhinpal.434@gmail.com>
2021-07-21 18:49:53 +0530

    Adding SpringBootServletInitializer

--- src/main/java/com/springapp/BankingApp/TransactionDetails/TransactionDetailsApplication.java
+++ src/main/java/com/springapp/BankingApp/TransactionDetails/TransactionDetailsApplication.java
@@ -2,6 +2,8 @@ package com.springapp.BankingApp.TransactionDetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +11,12 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class TransactionDetailsApplication {
+public class TransactionDetailsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TransactionDetailsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionDetailsApplication.class, args);
